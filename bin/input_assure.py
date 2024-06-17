8bb6d3dd1aec86ea51d1016e75001ca853317029
Eric <emarinier@gmail.com>
2024-06-17 14:24:34 -0500

    Updating input_assure

--- bin/input_assure.py
+++ bin/input_assure.py
@@ -4,6 +4,7 @@ import json
 import argparse
 import csv
 import gzip
+import sys
 
 
 def open_file(file_path, mode):
@@ -14,21 +15,25 @@ def open_file(file_path, mode):
         return open(file_path, mode)
 
 
-def check_inputs(json_file, sample_id, address, output_error_file):
+def check_inputs(json_file, sample_id, address, output_error_file, output_json_file):
     with open_file(json_file, "rt") as f:
         json_data = json.load(f)
 
     # Define a variable to store the match_status (True or False)
     match_status = sample_id in json_data
 
-    keys = list(json_data.keys())
-    original_key = keys[0]
-
     # Initialize the error message
     error_message = None
 
     # Check for multiple keys in the JSON file and define error message
-    if len(keys) > 1:
+    keys = list(json_data.keys())
+    original_key = keys[0] if keys else None
+
+    if len(keys) == 0:
+        error_message = f"{json_file} is completely empty!"
+        print(error_message)
+        sys.exit(1)
+    elif len(keys) > 1:
         # Check if sample_id matches any key
         if not match_status:
             error_message = f"No key in the MLST JSON file ({json_file}) matches the specified sample ID '{sample_id}'. The first key '{original_key}' has been forcefully changed to '{sample_id}' and all other keys have been removed."
@@ -55,7 +60,7 @@ def check_inputs(json_file, sample_id, address, output_error_file):
             writer.writerow([sample_id, keys, error_message])
 
     # Write the updated JSON data back to the original file
-    with open_file(json_file, "wt") as f:
+    with gzip.open(output_json_file, "wt") as f:
         json.dump(json_data, f, indent=4)
 
 
@@ -73,7 +78,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output_error", help="Path to the error report file.", required=True
     )
+    parser.add_argument(
+        "--output_json", help="Path to the MLST JSON file (gzipped).", required=True
+    )
 
     args = parser.parse_args()
 
-    check_inputs(args.input, args.sample_id, args.address, args.output_error)
+    check_inputs(
+        args.input, args.sample_id, args.address, args.output_error, args.output_json
+    )
